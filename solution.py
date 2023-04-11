d25f0ba9e8eba48f8375c425f4647a2fc8aea80f
IrinaShel <129631109+IrinaShel@users.noreply.github.com>
2023-04-12 01:40:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
+import warnings
 
+from hyppo.ksample import Energy, MMD, DISCO
+from scipy.stats import laplace, norm, ks_2samp, anderson_ksamp, cramervonmises_2samp
+from statsmodels.stats.weightstats import ztest
+from statsmodels.distributions.empirical_distribution import ECDF
 
 chat_id = 5351182285 # Ваш chat ID, не меняйте название переменной
 
@@ -8,4 +14,6 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    res = stats.cramervonmises_2samp(x, y)
+    Result = bool (res.pvalue<=0.06)
+    return Result # Ваш ответ, True или False
